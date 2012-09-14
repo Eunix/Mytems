ba892f45cffa0c840f4850b72396b3ac2caa5057
Evgeny Pavlov <evgeny@pavlov.name>
2012-09-14 16:09:05 +0400

    Started to test a string with box

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ActiveRecord::Base
   validates_presence_of :name
 
   belongs_to :box
+
+  before_create :parse_name
+
+  # We parse a name of box after @-symbol and categories after #-symbol.
+  # After that we remain only the name of an item
+  def parse_name
+
+  end
 end
--- test/unit/item_test.rb
+++ test/unit/item_test.rb
@@ -5,4 +5,10 @@ class ItemTest < ActiveSupport::TestCase
     item = Item.new
     assert !item.save
   end
+
+  test "should parse @ as box" do
+    item = Item.create :name => "new item @kitchen"
+    assert_equal item.name, "new item"
+    assert_equal item.box.name, "kitchen"
+  end
 end
