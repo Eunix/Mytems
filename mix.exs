9704de940deeb6e044a6d69c8ff3d203509aefac
Evgeny Pavlov <evgeny@pavlov.name>
2018-03-24 10:54:46 +0100

    Install edeliver and distillery

--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule Mytems.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:edeliver, "~> 1.4.5"},
+      {:distillery, "~> 1.0.0", warn_missing: false}
     ]
   end
 
